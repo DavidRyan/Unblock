d66233f81a03b08b5e9f4402a091b5fcb7e9b3e7
David <dryan31@gmail.com>
2011-10-26 18:34:31 -0500

    Initial Commit

---
+++ UB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UB
+//
+//  Created by David Ryan on 10/26/11.
+//  Copyright (c) 2011 DePaul University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UB
+//
+//  Created by David Ryan on 10/26/11.
+//  Copyright (c) 2011 DePaul University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
